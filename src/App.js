a48103838ae946e315172de524c714fd4cf1ce2f
joedesjunior <joedesjunior@gmail.com>
2020-03-10 10:44:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import profile from './assets/profile.png';
+
+function App(){
+    return <img src={profile} />,
+    return <h1></h1>
+    
+}
+
+export default App; 
